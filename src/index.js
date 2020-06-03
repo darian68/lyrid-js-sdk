a8a95886a1ecceb390ab00464de2981c49ba7993
darian_ung <darian.ung@uberfusion.com>
2020-06-03 14:30:10 +0700

    Basically finish the sdk functions

--- src/index.js
+++ src/index.js
@@ -6,112 +6,119 @@ global.Headers = fetch.Headers;
 
 class Lyrid {
   constructor(key, secret, token='') {
+
     this.key = key;
     this.secret = secret;
     this.token = token;
-  }
-  
-  async makeRequest(requestEndpoint, requestOptions) {
-    const response = await fetch(requestEndpoint, requestOptions);
-    const status = await response.status;
-    if (status >= 200 && status < 300) {
-        return await response.json();
-    } else {
-        return await response.text();
-    }
-  }
-  async execute(id, framework='', inputs='') {
     
+    this.getRequest = async function (url) {
+      const token = await this.getToken();
+      const lyridHeaders = new Headers();
+      lyridHeaders.append("Content-Type", "application/json");
+      lyridHeaders.append("Authorization", "Bearer " + token);
+      const requestOptions = {
+        method: 'GET',
+        headers: lyridHeaders,
+        redirect: 'follow'
+      };
+      const response = await fetch(url, requestOptions);
+      console.log(response);
+      const status = await response.status;
+      console.log(status);
+      if (status >= 200 && status < 300) {
+        return await response.json();
+      } else {
+        const resBody = await response.text();
+        if (resBody == "Expired authorization token") {
+          await this.refreshToken();
+          // TODO need to cover the endless recursive
+          return await this.getRequest(url);
+        }
+      }
+    };
+
+    this.getToken = async function () {
+      let token = this.token;
+      if (!token) {
+        token = await this.refreshToken();
+      }
+      return token;
+    };
+
+    this.refreshToken = async function () {
+      let token = '';
+      const requestOptions = {
+        method: 'POST',
+        headers: new Headers().append("Content-Type", "application/json"),
+        body: JSON.stringify({"key": this.key,"secret": this.secret}),
+        redirect: 'follow'
+      };
+      const response = await fetch("https://api.lyrid.io/auth", requestOptions);
+      const status = await response.status;
+      if (status >= 200 && status < 300) {
+        const json = await response.json();
+        this.token = await json.token;
+        token = this.token;
+      }
+      return token;
+    };
   }
 
-  async getToken() {
-    let token = this.token;
-    if (!token) {
-      token = await this.refreshToken();
-    }
-    return token;
-  }
-  
-  async refreshToken() {
-    let token = '';
+  async execute(id, framework='', inputs='') {
+    console.log("executing a function");
+    const token = await this.getToken();
+    const requestEndpoint = lyridEndpoint + 'api/serverless/app/execute/'+ id + "/" + framework;
+    const lyridHeaders = new Headers();
+    lyridHeaders.append("Content-Type", "application/json");
+    lyridHeaders.append("Authorization", "Bearer " + token);
     const requestOptions = {
       method: 'POST',
-      headers: new Headers().append("Content-Type", "application/json"),
-      body: JSON.stringify({"key": this.key,"secret": this.secret}),
+      headers: lyridHeaders,
+      body: inputs,
       redirect: 'follow'
     };
-    const response = await fetch("https://api.lyrid.io/auth", requestOptions);
+    const response = await fetch(requestEndpoint, requestOptions);
+    console.log(response);
     const status = await response.status;
-    if (status >= 200 && status < 300) {
-      const json = await response.json();
-      this.token = await json.token;
-      token = this.token;
-    }
-    return token;
+      console.log(status);
+      if (status >= 200 && status < 300) {
+        return await response.json();
+      } else {
+        const resBody = await response.text();
+        if (resBody == "Expired authorization token") {
+          await this.refreshToken();
+          // TODO need to cover the endless recursive
+          return await this.execute(id, framework, inputs);
+        }
+      }
   }
   
-  // /api/serverless/app/get
+  // api/serverless/app/get
   async getApps() {
     console.log("Get apps");
-    const token = await this.getToken();
-    const lyridHeaders = new Headers();
-    lyridHeaders.append("Content-Type", "text/plain");
-    lyridHeaders.append("Authorization", "Bearer " + token);
     const requestEndpoint = lyridEndpoint + 'api/serverless/app/get';
-    const requestOptions = {
-      method: 'GET',
-      headers: lyridHeaders,
-      redirect: 'follow'
-    };
-    const response = await fetch(requestEndpoint, requestOptions);
-    console.log(response);
-    const status = await response.status;
-    console.log(status);
-    if (status >= 200 && status < 300) {
-      return await response.json();
-    } else {
-      const resBody = await response.text();
-      if (resBody == "Expired authorization token") {
-        const token = await this.refreshToken();
-        return await this.getApps();
-      }
-    }
+    return await this.getRequest(requestEndpoint);
   }
 
-  // /api/serverless/app/get/{appid}
+  // api/serverless/app/get/{appid}
   async getModules(appId) {
     console.log("Get Modules");
-    const token = await this.getToken();
-    const lyridHeaders = new Headers();
-    lyridHeaders.append("Content-Type", "text/plain");
-    lyridHeaders.append("Authorization", "Bearer " + token);
     const requestEndpoint = lyridEndpoint + 'api/serverless/app/get/' + appId;
-    const requestOptions = {
-      method: 'GET',
-      headers: lyridHeaders,
-      redirect: 'follow'
-    };
-    const response = await fetch(requestEndpoint, requestOptions);
-    console.log(response);
-    const status = await response.status;
-    console.log(status);
-    if (status >= 200 && status < 300) {
-      return await response.json();
-    } else {
-      const resBody = await response.text();
-      if (resBody == "Expired authorization token") {
-        const token = await this.refreshToken();
-        return await this.getModules(appId);
-      }
-    }
+    return await this.getRequest(requestEndpoint);
   }
   
+  // api/serverless/app/get/{appid}/{moduleid}
   async getRevisions(appId, moduleId) {
-    
+    console.log("Get Revisions");
+    const requestEndpoint = lyridEndpoint + 'api/serverless/app/get/' + appId + '/' + moduleId;
+    return await this.getRequest(requestEndpoint);
   }
 
+  // api/serverless/app/get/{appid}/{moduleid}/{revisionid}
   async getFunctions(appId, moduleId, revisionId) {
-    
+    console.log("Get functions");
+    const requestEndpoint = lyridEndpoint + 'api/serverless/app/get/' + appId + '/' + moduleId+ '/' + revisionId;
+    return await this.getRequest(requestEndpoint);
   }
 
 }
--- test/index.js
+++ test/index.js
@@ -1,11 +1,17 @@
 import * as assert from 'assert';
 import Lyrid from '../src/index';
 
-const lc = new Lyrid('RsliNzH8xQrdVIWJqOwd', 'bJq0YT7CRNKz4p0xh8rnQJB4VfYNwINEI3zlFJ67V25VGd8eXZ', "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1ETkJPVGd6UTBNMVFUQTBNRGMzTlRjeE1EZzJNRVkwT0RVNFF6TkdNVGsxUTBKRVFURkNNQSJ9.eyJpc3MiOiJodHRwczovL2x5cmlkLmF1dGgwLmNvbS8iLCJzdWIiOiJRU3BBTzZ4R1h6Mnp3RTA4UXM4cndxT1F4VEpoeW9DNEBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9seXJpZC5pby9hY2Nlc3MvYXV0aCIsImlhdCI6MTU5MTA4NzU2NiwiZXhwIjoxNTkxMTczOTY2LCJhenAiOiJRU3BBTzZ4R1h6Mnp3RTA4UXM4cndxT1F4VEpoeW9DNCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.cVvmFFO0VtaeYFFV7R_XROq8iFGFb4alzKk5dawU97b1_diqlAc3-WrjHjzH0BTKvhM24IzksT7pPunNv6FyQiAGJVWvF8lrks9NoDS66nyu09wAYv5EdiMW2uo3BY2ZGaXg0o56pachlcjYZven7fEA4F1CN46eNH-gO28IUDClKoS41xvW2xCFPTcPafQGQdxrI98vjxzr095DSN322pWtnyv3lRa_XZlxqKujh9KYaeoEJWvJ1R-ZIO3KPvCwd8CW8vTbIGBiPw0Ondjt9uz8sZb6o8g9tbkkxh-wny6LXNKMVamsfTeCBdJKzSPDpDzODNpO8E1bDkro2i3Z3w");
+//const lc = new Lyrid('RsliNzH8xQrdVIWJqOwd', 'bJq0YT7CRNKz4p0xh8rnQJB4VfYNwINEI3zlFJ67V25VGd8eXZ', "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1ETkJPVGd6UTBNMVFUQTBNRGMzTlRjeE1EZzJNRVkwT0RVNFF6TkdNVGsxUTBKRVFURkNNQSJ9.eyJpc3MiOiJodHRwczovL2x5cmlkLmF1dGgwLmNvbS8iLCJzdWIiOiJRU3BBTzZ4R1h6Mnp3RTA4UXM4cndxT1F4VEpoeW9DNEBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9seXJpZC5pby9hY2Nlc3MvYXV0aCIsImlhdCI6MTU5MTA4NzU2NiwiZXhwIjoxNTkxMTczOTY2LCJhenAiOiJRU3BBTzZ4R1h6Mnp3RTA4UXM4cndxT1F4VEpoeW9DNCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.cVvmFFO0VtaeYFFV7R_XROq8iFGFb4alzKk5dawU97b1_diqlAc3-WrjHjzH0BTKvhM24IzksT7pPunNv6FyQiAGJVWvF8lrks9NoDS66nyu09wAYv5EdiMW2uo3BY2ZGaXg0o56pachlcjYZven7fEA4F1CN46eNH-gO28IUDClKoS41xvW2xCFPTcPafQGQdxrI98vjxzr095DSN322pWtnyv3lRa_XZlxqKujh9KYaeoEJWvJ1R-ZIO3KPvCwd8CW8vTbIGBiPw0Ondjt9uz8sZb6o8g9tbkkxh-wny6LXNKMVamsfTeCBdJKzSPDpDzODNpO8E1bDkro2i3Z3w");
+const lc = new Lyrid('RsliNzH8xQrdVIWJqOwd', 'bJq0YT7CRNKz4p0xh8rnQJB4VfYNwINEI3zlFJ67V25VGd8eXZ');
 //const response = lc.getToken();
+/*
 lc.getApps().then(data =>{
   console.log(data)
 });
-lc.getModules('56edc263-b0c5-4672-b6cf-ca45a5c4e033').then(data =>{
+lc.getRevisions('56edc263-b0c5-4672-b6cf-ca45a5c4e033','3ae6b878-30f3-4729-aa92-b1bfd5c376a9').then(data =>{
+  console.log(data)
+});*/
+const input = JSON.stringify({"InputSample":"Hello"});
+lc.execute('e7672cd0-ed0d-44ee-9068-c30feb5c7a8a', "LYR", input).then(data =>{
   console.log(data)
 });
